2d3c349b74a81784894c29ba19c97f61603c8b0e
igordami <igordamiao.contato@outlook.com>
2022-04-16 15:10:12 -0300

    first commit

---
+++ src/com/Dio.java
@@ -0,0 +1,10 @@
+package com;
+
+import base.Order ;
+public class Dio {
+    public static void main(String[] args) {
+       Order  Order = new Order("1515");
+        System.out.println(Order);
+    }
+
+}
